b0300049b3556403db87fda253434d024b660454
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2023-09-19 00:24:18 +0100

    Refactoring the code

--- printf.c
+++ printf.c
@@ -1,54 +1,54 @@
-/**
-  *File: printf.c
-  * _printf: Custom implementation of the printf function.
-  * Author: Opara Vivian and Emmanuel Obiajulu Okoye
-  *
-  * @format: a character string containing format specifiers
-  * @...: variable number of arguments to be formatted
-  * Return: 0
-  */
 #include "main.h"
+/**
+ *File: printf.c
+ * _printf: Custom implementation of the printf function.
+ * Author: Opara Vivian and Emmanuel Obiajulu Okoye
+ *
+ * @format: a character string containing format specifiers
+ * @...: variable number of arguments to be formatted
+ * Return: 0
+ */
 int _printf(const char *format, ...)
 {
-int character, counter = 0;
-char *string;
-va_list args;
-va_start(args, format);
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-if (*format == 'c')
-{
-character = va_arg(args, int);
-putchar(character);
-counter++;
-}
-else if (*format == 's')
-{
-string = va_arg(args, char *);
-while (*string != '\0')
-{
-putchar(*string);
-string++;
-counter++;
-}
-}
-else if (*format == '%')
-{
-putchar('%');
-counter++;
-}
-else
-{
-putchar(*format);
-counter++;
-}
-format++;
-}
-va_end(args);
-return (counter);
-}
-return (0);
+  int character, counter = 0;
+  char *string;
+  va_list args;
+  va_start(args, format);
+  while (*format != '\0')
+  {
+    if (*format == '%')
+    {
+      format++;
+      if (*format == 'c')
+      {
+        character = va_arg(args, int);
+        putchar(character);
+        counter++;
+      }
+      else if (*format == 's')
+      {
+        string = va_arg(args, char *);
+        while (*string != '\0')
+        {
+          putchar(*string);
+          string++;
+          counter++;
+        }
+      }
+      else if (*format == '%')
+      {
+        putchar('%');
+        counter++;
+      }
+      else
+      {
+        putchar(*format);
+        counter++;
+      }
+      format++;
+    }
+    va_end(args);
+    return (counter);
+  }
+  return (0);
 }
--- test/test2.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-#include <stdio.h> 
-int main() 
-{
-	int num = 50;
-	_printf("This is a number: %d\n", num);
-	printf("This is a number: %d\n", num);
-	return (0);
-}
