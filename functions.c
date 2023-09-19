ce7565cf58c5b4581caa211858d7d94285c7c988
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2023-09-19 01:32:18 +0100

    Refactoring the code

--- functions.c
+++ functions.c
@@ -24,8 +24,23 @@ char *str = va_arg(args, char *);
 int len = 0;
 
 while (str[len])
-len++;
+    len++;
 
 write(1, str, len);
 return (len);
+}
+
+/**
+ * print_int - Print an integer.
+ * @args: va_list containing the integer to print.
+ *
+ * Return: Number of characters printed.
+ */
+static int print_int(va_list args)
+{
+int num = va_arg(args, int);
+char buffer[12]; // Buffer to store the integer as a string
+int len = sprintf(buffer, "%d", num);
+write(1, buffer, len);
+return len;
 }
--- printf.c
+++ printf.c
@@ -1,47 +1,45 @@
 #include "main.h"
 
-
-
 /**
- * _printf - Custom printf function to handle 'c', 's', and '%'.
+ * _printf - Custom printf function to handle 'c', 's', 'd', 'i', and '%'.
  * @format: Format specifier string.
  *
  * Return: Number of characters printed (excluding null byte).
  */
 int _printf(const char *format, ...)
 {
-  va_list args;
-  int count = 0;
+va_list args;
+int count = 0;
 
-  va_start(args, format);
+va_start(args, format);
 
-  while (*format)
-  {
-    if (*format == '%')
-    {
-      if ((*(format + 1) == 'c' || *(format + 1) == 's' || *(format + 1) == '%'))
-      {
-        format++;
-        if (*format == 'c')
-          count += print_char(args);
-        else if (*format == 's')
-          count += print_str(args);
-        else if (*format == '%')
-        {
-          write(1, "%", 1);
-          count++;
-        }
-      }
-    }
+while (*format)
+{
+if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's' ||
+                    *(format + 1) == 'd' || *(format + 1) == 'i' ||
+                    *(format + 1) == '%'))
+{
+format++;
+if (*format == 'c')
+count += print_char(args);
+else if (*format == 's')
+count += print_str(args);
+else if (*format == 'd' || *format == 'i')
+count += print_int(args);
+else if (*format == '%')
+{
+write(1, "%", 1);
+count++;
+}
+}
+else
+{
+write(1, format, 1);
+count++;
+}
+format++;
+}
 
-    else
-    {
-      write(1, format, 1);
-      count++;
-    }
-    format++;
-  }
-
-  va_end(args);
-  return (count);
+va_end(args);
+return (count);
 }
