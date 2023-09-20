00eed07fc44ab91877fa3af24751b32973568fd3
Opara <oparavivian720@gmail.com>
2023-09-19 21:35:00 -0400

    update

--- my_second_function.c
+++ my_second_function.c
@@ -4,12 +4,10 @@
   *print_binary - convert and print unsigned
   *		  integar in binary
   *@num: number
-  *Return: printed_c
+  *Return: void
   */
-int print_binary(unsigned int num);
-int print_binary(unsigned int num)
+void print_binary(unsigned int num)
 {
-	 int printed_c = 0;
 	 unsigned int m = 1u << (sizeof(unsigned int) * 8 - 1);
 
 	while (m > 0)
@@ -17,8 +15,6 @@ int print_binary(unsigned int num)
 		char b = (num & m) ? '1' : '0';
 
 		write(1, &b, 1);
-		printed_c++;
 		m >>= 1;
 	}
-	return (printed_c);
 }
